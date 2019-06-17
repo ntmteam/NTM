a53696a8ea87ba231f86a6fb363aa22bb9a5091e
Chemodanchick <43390825+Chemodanchick@users.noreply.github.com>
2019-06-17 23:55:21 +0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -4,10 +4,20 @@ import torch.nn as nn
 import torch.nn.functional as F 
 
 class Controller(nn.Module):
-	def __init__(self, input , output, hid ):
+	def __init__(self, input_size , output_size, hidden_size ):
 		super(Controller, self).__init__()
-		self.fc1 = nn.Linear(input, hid)
-		self.fc2 = nn.Linear(input, hid )
+		self.fc1 = nn.Linear(input, hidden_size)
+		self.fc2 = nn.Linear(input, hidden_size)
+		self.reset_parameters
+	
+	def reset_parameters(self):
+		nn.init.xavier_uniform_(self.fc1.weight, gain = 1.4)
+		nn.init.xavier_uniform_(self.fc2.weight, gain = 1.4)
+		nn.init.normal_(self.fc1.bias, std=0.01)
+		nn.init.normal_(self.fc2.bias, std=0.01)
+
+	
+       
 		
 	def forward(self, x, last_read):
 		x = torch.cat((x, last_read), dim = 1)
