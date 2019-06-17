617d408f0ffacebe6ff770b4c51e45b7ce82fe5e
Chemodanchick <43390825+Chemodanchick@users.noreply.github.com>
2019-06-18 02:38:17 +0300

    Update ntm.py

--- ntm.py
+++ ntm.py
@@ -1,1 +1,10 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+from memory import ReadHead, WriteHead
+from controller import Controller
 
+
+class NTM(nn.Module):
+  def __init__(self, M, N,)
