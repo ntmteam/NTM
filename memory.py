d767b9dc21f029fa1e601dcbcfba800e80f81346
Chemodanchick <43390825+Chemodanchick@users.noreply.github.com>
2019-06-17 23:29:31 +0300

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,2 +1,18 @@
 import torch
-from 
+from  torch import nn
+import torch.nn.functional as А
+
+class Memory(nn.Module):
+  def __init__(self):
+    super(Memory, self).__init__()
+    
+    self.N = N
+    self.M = M
+    self.read = 
+    self.write = 
+    self.w_last = []
+    
+   def get.weights(self):
+    return self.w_last
+  
+  
