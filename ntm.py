41a1eda482bb70c6d9a7f5cbb4b510350c69353e
Chemodanchick <43390825+Chemodanchick@users.noreply.github.com>
2019-06-18 02:42:04 +0300

    Update ntm.py

--- ntm.py
+++ ntm.py
@@ -7,4 +7,14 @@ from controller import Controller
 
 
 class NTM(nn.Module):
-  def __init__(self, M, N,)
+  def __init__(self, M, N,input_size , output_size, controller_out_dim, controller_hid_dim ):
+    super(NTM, self).__init__()
+    
+    self.input_size = input_size
+    self.outputs_size = outputs_size
+    self.M = M
+    self.N = N
+    
+    
+    
+    
