b6fa2b1355b39edb50f1f21f9b456e333857b8e9
Chemodanchick <43390825+Chemodanchick@users.noreply.github.com>
2019-06-20 11:31:51 +0300

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,22 +1,26 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F 
+from torch import nn
+import torch.nn.functional as F
+
 
 class Controller(nn.Module):
-	def __init__(self, input_size , output_size, hidden_size ):
-		super(Controller, self).__init__()
-		self.fc1 = nn.Linear(input, hidden_size)
-		self.fc2 = nn.Linear(input, hidden_size)
-		self.reset_parameters
-	
-	def reset_parameters(self):
-		nn.init.xavier_uniform_(self.fc1.weight, gain = 1.4)
-		nn.init.xavier_uniform_(self.fc2.weight, gain = 1.4)
-		nn.init.normal_(self.fc1.bias, std=0.01)
-		nn.init.normal_(self.fc2.bias, std=0.01)
+    def __init__(self, input_size, output_size, hidden_size):
+        super(Controller, self).__init__()
 
-	def forward(self, x, last_read):
-		x = torch.cat((x, last_read), dim = 1)
-		x = torch.sigmoid(self.fc1(x))
-		x = torch.sigmoid(self.fc2(x))
-		return x
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.fc1.weight, gain=1.4)
+        nn.init.normal_(self.fc1.bias, std=0.01)
+
+        nn.init.xavier_uniform_(self.fc2.weight, gain=1.4)
+        nn.init.normal_(self.fc2.bias, std=0.01)
+
+    def forward(self, x, last_read):
+
+        x = torch.cat((x, last_read), dim=1)
+        x = torch.sigmoid(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        return x
