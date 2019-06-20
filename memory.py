3400f0e8c176e0c7daa1fb07e2c03b842ddf0ba2
Chemodanchick <43390825+Chemodanchick@users.noreply.github.com>
2019-06-20 11:30:16 +0300

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,71 +1,72 @@
 import torch
-from  torch import nn
-import torch.nn.functional as F 
+from torch import nn
+import torch.nn.functional as F
+
 
 class Memory(nn.Module):
-  def __init__(self):
-    super(Memory, self).__init__()
-    
-    self.N = N
-    self.M = M
-    self.read = self.N + 1 + 1 + 3 + 1
-    self.write = self.N + 1 + 1 + 3 + 1 + self.N + self.N
-    self.w_last = []
-  def get.weights(self):
-    return self.w_last
-  
-  def reset_memory(self):
-    self.w_last = []
-    self.w_last.append(torch.zeros([1, self.M], dtype=torch.float32))
-    
-    
-        # key  : k, key vector for content-based addressing
-        # stren: β, key strength scalar
-        # gate : g, interpolation gate scalar
-        # shift: s, shift weighting vector
-        # sharp: γ, sharpening scalar
-    
-  def address(self,key, stren, gate, shift, sharp, memory, w_last):
-    wc = self._similarity(key, stren)
-    wg = self._interpolate(wc, gate, w_last)
-    w_tmp = self._shift(wg, shift)
-    w = self._sharpen(w_tmp, sharp)
-    
-    return w
-  
-  def _similarity(self, key, stren, memory):
-    w = F.cosine_similarity(memory, k, -1, 1e-16)
-    w = F.softmax(stren * w, dim=-1)
-    return w
-  
-  def _interpolate(self, wc, gate, w_last):
-    return gate * wc + (1 - gate) * w_last
+    def __init__(self, M, N, controller_out):
+        super(Memory, self).__init__()
 
-  def _shift(self, wg, shift):
-    result = torch.zeros(wg.size())
-    result = _convolve(wg, shift)
-    return result
-      
-  def _sharpen(self, w_tmp, sharp):
-    w = w_tmp**sharp
-    w = torch.div(w, torch.sum(w, dim = -1) + 1e-16)
-    return w
-  
+        self.N = N
+        self.M = M
+        self.read_lengths = self.N + 1 + 1 + 3 + 1
+        self.write_lengths = self.N + 1 + 1 + 3 + 1 + self.N + self.N
+        self.w_last = []
+        self.reset_memory()
+
+    def get_weights(self):
+        return self.w_last
+
+    def reset_memory(self):
+        self.w_last = []
+        self.w_last.append(torch.zeros([1, self.M], dtype=torch.float32))
+
+    def address(self, key, stren, gate, shift, sharp, memory, w_last):
+        wc = self._similarity(key, stren, memory)
+
+        wg = self._interpolate(wc, gate, w_last)
+        w_t = self._shift(wg, shift)
+        w = self._sharpen(w_t, sharp)
+
+        return w
+
+    def _similarity(self, key, stren, memory):
+
+        w = F.cosine_similarity(memory, key, -1, 1e-16)
+        w = F.softmax(stren * w, dim=-1)
+        return w
+
+    def _interpolate(self, wc, gate, w_last):
+        return gate * wc + (1 - gate) * w_last
+
+    def _shift(self, wg, shift):
+        result = torch.zeros(wg.size())
+        result = _convolve(wg, shift)
+        return result
+
+    def _sharpen(self, w_t, sharp):
+        w = w_t ** sharp
+        w = torch.div(w, torch.sum(w, dim=-1) + 1e-16)
+        return w
+
+
 class ReadHead(Memory):
-  def __init__(self, M, N, controller_out):
+
+    def __init__(self, M, N, controller_out):
         super(ReadHead, self).__init__(M, N, controller_out)
 
-        self.fc_read = nn.Linear(controller_out, self.read_lengths)
-        self.reset_parameters();
 
-  def reset_parameters(self):
+        self.fc_read = nn.Linear(controller_out, self.read_lengths)
+        self.reset_parameters()
+
+    def reset_parameters(self):
         nn.init.xavier_uniform_(self.fc_read.weight, gain=1.4)
         nn.init.normal_(self.fc_read.bias, std=0.01)
 
-  def read(self, memory, w):
+    def read(self, memory, w):
         return torch.matmul(w, memory)
 
-  def forward(self, x, memory):
+    def forward(self, x, memory):
         param = self.fc_read(x)
         key, stren, gate, shift, sharp = torch.split(param, [self.N, 1, 1, 3, 1], dim=1)
 
@@ -83,17 +84,19 @@ class ReadHead(Memory):
 
 class WriteHead(Memory):
 
-  def __init__(self, M, N, controller_out):
+    def __init__(self, M, N, controller_out):
         super(WriteHead, self).__init__(M, N, controller_out)
+
 
         self.fc_write = nn.Linear(controller_out, self.write_lengths)
         self.reset_parameters()
 
-  def reset_parameters(self):
+    def reset_parameters(self):
         nn.init.xavier_uniform_(self.fc_write.weight, gain=1.4)
         nn.init.normal_(self.fc_write.bias, std=0.01)
 
-  def write(self, memory, w, e, a):   
+    def write(self, memory, w, e, a):
+        """write to memory (according to section 3.2)."""
         w = torch.squeeze(w)
         e = torch.squeeze(e)
         a = torch.squeeze(a)
@@ -106,10 +109,10 @@ class WriteHead(Memory):
 
         return memory_update
 
-  def forward(self, x, memory):
+    def forward(self, x, memory):
         param = self.fc_write(x)
 
-       key, stren, gate, shift, sharp, a, e = torch.split(param, [self.N, 1, 1, 3, 1, self.N, self.N], dim=1)
+        key, stren, gate, shift, sharp, a, e = torch.split(param, [self.N, 1, 1, 3, 1, self.N, self.N], dim=1)
 
         key = torch.tanh(key)
         stren = F.softplus(stren)
